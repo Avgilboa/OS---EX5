8aece84ce2932d05ec4b0016f053536f9e1df231
shmooel28 <shmooel28@gmail.com>
2023-01-26 13:48:12 +0200

    func 1-2

--- pipeLine.cpp
+++ pipeLine.cpp
@@ -20,6 +20,12 @@ public:
   ActiveObject1(std::shared_ptr<ActiveObject> next) : next_(next) {}
   void SendMessage(std::unique_ptr<My_string> data) override {
     // Perform operation 1 on data
+    std::string curr = "";
+    for(int i=0; i<data.get()->current.length(); i++){
+      if(data.get()->current[i] >= 32 && data.get()->current[i] <= 126)
+        curr += data.get()->current[i];
+    }
+    data.get()->current = curr;
     next_->SendMessage(std::move(data));
   }
 
@@ -29,8 +35,19 @@ class ActiveObject2 : public ActiveObject {
 public:
   ActiveObject2(std::shared_ptr<ActiveObject> next) : next_(next) {}
   void SendMessage(std::unique_ptr<My_string> data) override {
-    std::cout << "Perform operation 2 on data" << std::endl;
-    data.get()->original = "world";
+    std::string curr = "";
+    for(int i=0; i<data.get()->current.length(); i++){
+      if(data.get()->current[i] >= 'a' && data.get()->current[i] <= 'z'){
+        curr += data.get()->current[i] - 32;
+      }
+      else if(data.get()->current[i] >= 'A' && data.get()->current[i] <= 'Z'){
+        curr += data.get()->current[i];
+      }
+      else{
+        curr += ' ';
+      }
+    }
+    data.get()->current = curr;
     next_->SendMessage(std::move(data));
   }
 private:
@@ -41,7 +58,7 @@ class ActiveObject3 : public ActiveObject {
 public:
   ActiveObject3() {}
   void SendMessage(std::unique_ptr<My_string> data) override {
-    std::cout << "Perform operation 3 on data" << data.get()->original << std::endl;
+    std::cout << "Perform operation 3 on data " << data.get()->current << std::endl;
   }
 };
 
@@ -50,6 +67,6 @@ int main() {
   auto active_object2 = std::make_shared<ActiveObject2>(active_object3);
   auto active_object1 = std::make_shared<ActiveObject1>(active_object2);
 
-  std::unique_ptr<My_string> data = std::make_unique<My_string>("hello");
+  std::unique_ptr<My_string> data = std::make_unique<My_string>("h\te4\n5ll!~`o");
   active_object1->SendMessage(std::move(data));
 }
